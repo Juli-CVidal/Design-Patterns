2f7f791098e146f21306533c25140f72df5c0a43
Juli-CVidal <juli.vidal.019@gmail.com>
2022-11-21 16:59:06 -0300

    Another small improves

--- STRATEGY/src/Utils/Menu.java
+++ STRATEGY/src/Utils/Menu.java
@@ -1,8 +1,12 @@
 package Utils;
 
+import java.util.Arrays;
+import java.util.List;
+
 import Constants.Constants;
 import Entities.ShoppingCart;
 import PaymentMethods.CreditCardPayment;
+import PaymentMethods.Payment;
 import PaymentMethods.PaypalPayment;
 import Services.Input;
 import Services.ItemService;
@@ -17,7 +21,7 @@ public class Menu {
 			System.out.println(Constants.MAIN_MENU_OPTIONS);
 			option = Input.askInteger(Constants.ASK_OPTION);
 			System.out.println("");
-			
+
 			if (0 >= option || EXIT_OPTION <= option) {
 				continue;
 			}
@@ -26,51 +30,48 @@ public class Menu {
 			case 1: // ADD AN ITEM
 				cart.addItem(ItemService.createItem());
 				break;
-			
+
 			case 2: // DELETE AN ITEM
 				System.out.println(cart);
 				cart.removeItem(Input.askString(Constants.ASK_ITEM_NAME));
 				break;
-			case 3: //PAY
+			case 3: // PAY
 				pay(cart);
 				break;
 			}
 
-			
 		} while (EXIT_OPTION != option);
 
 	}
 	
-	
-	public static void pay(ShoppingCart cart) {
-		Integer option;
-		final Integer EXIT_OPTION = 3;
-		
+
+	private static void pay(ShoppingCart cart) {
 		if (cart.getItems().isEmpty()) {
 			System.out.println(Constants.EMPTY_CART);
 			return;
 		}
-		
 		System.out.println(cart);
+		
+		cart.pay(selectPaymentMethod());
+		cart.getItems().clear();
+	}
+	
+	private static Payment selectPaymentMethod() {
+		final List<Payment> PAYMENT_METHODS = Arrays.asList(new CreditCardPayment(), new PaypalPayment());
+		Integer option;
+		final Integer EXIT_OPTION = 3;
+		
+		
 		do {
 			System.out.println(Constants.PAYMENT_OPTIONS);
 			option = Input.askInteger(Constants.ASK_OPTION);
 			System.out.println("");
-
+			
 			if (0 >= option || EXIT_OPTION <= option) {
 				continue;
 			}
-			
-			switch(option) {
-			case 1: //CREDIT CARD
-				cart.pay(new CreditCardPayment());
-				break;
-			case 2: //PAYPAL
-				cart.pay(new PaypalPayment());
-				break;
-			}
-			cart.getItems().clear();
-		}while (EXIT_OPTION != option);
+		} while (EXIT_OPTION != option);
+		
+		return PAYMENT_METHODS.get(option);
 	}
-	
 }
